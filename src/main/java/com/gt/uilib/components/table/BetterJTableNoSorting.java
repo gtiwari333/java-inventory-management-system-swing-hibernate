613e4bdad598d0bf2fbdb7e482d7e3cf4a484593
haka913 <43568065+haka913@users.noreply.github.com>
2020-06-03 10:03:38 +0900

    1. Template method pattern 2. com.gt.uilib.components.table.BetterJTable.java and com.gt.uilib.components.table.BetterJTableNoSorting.java 3. There are duplicate methods and fields in two java classes. So we define the skeleton of an algorithm in superclass, and deferring some steps to subclasses.

--- src/main/java/com/gt/uilib/components/table/BetterJTableNoSorting.java
+++ src/main/java/com/gt/uilib/components/table/BetterJTableNoSorting.java
@@ -6,59 +6,29 @@ import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
-public class BetterJTableNoSorting extends JTable {
+public class BetterJTableNoSorting extends AbstractBetterJTable {
 
-    private static final Color EVEN_ROW_COLOR = new Color(240, 255, 250);
-    private static final Color TABLE_GRID_COLOR = new Color(0xd9d9d9);
 
-    public BetterJTableNoSorting(TableModel dm) {
-        super(dm);
-        init();
-        setFillsViewportHeight(true);
-        setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-//		setEditable(false);
-        putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
-    }
+	public BetterJTableNoSorting(TableModel dm) {
+		super(dm);
+		init();
+		setFillsViewportHeight(true);
+		setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		// setEditable(false);
+		putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
+	}
 
-    public void adjustColumns() {
-//		packAll();
-    }
+	@Override
+	public void adjustColumns() {
+		// TODO Auto-generated method stub
+		// packAll();
+	}
+	@Override
+	public final Class<?> getColumnClass(int column) {
+		return (column == 0 || column == 1) ? Integer.class : Object.class;
+	}
 
-    @Override
-    public final Class<?> getColumnClass(int column) {
-        return (column == 0 || column == 1) ? Integer.class : Object.class;
-    }
 
-    public final String getToolTipText(MouseEvent e) {
-        int row = rowAtPoint(e.getPoint());
-        int column = columnAtPoint(e.getPoint());
-        try {
-            Object value = getValueAt(row, column);
-            return value == null ? "" : value.toString();
-        } catch (Exception ex) {
-            return "";
-        }
-    }
 
-    private void init() {
-        setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-        getTableHeader().setReorderingAllowed(false);
-        setOpaque(false);
-        setGridColor(TABLE_GRID_COLOR);
-        setShowGrid(true);
-    }
-
-    @Override
-    public final Component prepareRenderer(TableCellRenderer tcr, int row, int column) {
-        Component c = super.prepareRenderer(tcr, row, column);
-        if (isRowSelected(row)) {
-            c.setForeground(getSelectionForeground());
-            c.setBackground(getSelectionBackground());
-        } else {
-            c.setForeground(getForeground());
-            c.setBackground((row % 2 == 0) ? EVEN_ROW_COLOR : getBackground());
-        }
-        return c;
-    }
 
 }
